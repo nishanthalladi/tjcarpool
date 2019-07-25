11dbcca6cca07790a752185529855b0d469c133a
nishanthalladi <nishanthalladi01@gmail.com>
2019-07-25 12:29:22 -0400

    buttons work sort of

--- index.js
+++ index.js
@@ -4,13 +4,28 @@ var hbs = require('hbs');
 app.set('view engine', 'hbs');
 
 app.set("port", process.env.PORT || 8080);
-app.set('view engine', 'hbs');
 app.set('trust proxy', 1)
 
 app.get("/", function(req, res){
-    res.send("carpool time");
+    // res.sendFile(__dirname + '/index.html');
+    res.render('home');
+});
+
+app.get("/request_pool", function(req, res){
+    // res.sendFile(__dirname + '/index.html');
+    res.render('request');
+});
+
+app.get("/host_pool", function(req, res){
+    // res.sendFile(__dirname + '/index.html');
+    res.render('home');
+});
+
+app.get("/view_pools", function(req, res){
+    // res.sendFile(__dirname + '/index.html');
+    res.render('home');
 });
 
 var listener = app.listen(app.get('port'), function() {
-  console.log( 'Express server started on port: '+listener.address().port );
+  console.log( 'Express server started on port: '+listener.address().port);
 });
